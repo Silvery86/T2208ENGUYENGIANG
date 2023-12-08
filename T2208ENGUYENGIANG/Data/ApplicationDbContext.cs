f9878319d4931c2bfaaa960923da5346536e5d4b
giang8692@gmail.com <giang8692@gmail.com>
2023-12-08 20:50:49 +0700

    Add project files.

---
+++ T2208ENGUYENGIANG/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace T2208ENGUYENGIANG.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<T2208ENGUYENGIANG.Models.Department>? Department { get; set; }
+        public DbSet<T2208ENGUYENGIANG.Models.Employee>? Employee { get; set; }
+    }
+}
---
+++ T2208ENGUYENGIANG/Models/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace T2208ENGUYENGIANG.Models
+{
+    public class Report
+    { 
+         public Department department { get; set; }
+         public List<Employee> employees { get; set; }
+    
+    }
+}
